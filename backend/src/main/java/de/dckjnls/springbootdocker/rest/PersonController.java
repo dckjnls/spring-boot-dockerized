129c6a93a3d29bf3c43d670d66199ab532e75157
Dack Janiels <dckjnls@gmail.com>
2021-08-18 18:47:18 +0200

    Added feature for getting one Person

--- backend/src/main/java/de/dckjnls/springbootdocker/rest/PersonController.java
+++ backend/src/main/java/de/dckjnls/springbootdocker/rest/PersonController.java
@@ -1,14 +1,12 @@
 package de.dckjnls.springbootdocker.rest;
 
 import de.dckjnls.springbootdocker.business.IPersonService;
+import de.dckjnls.springbootdocker.exception.DataNotFoundException;
 import de.dckjnls.springbootdocker.persistence.entity.PersonEty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,5 +30,10 @@ public class PersonController {
     @GetMapping("/people")
     public ResponseEntity<List<PersonEty>> getAllPeople() {
         return new ResponseEntity<>(personSvc.readAllPeople(), HttpStatus.OK);
+    }
+
+    @GetMapping("/person/{id}")
+    public ResponseEntity<PersonEty> getPersonById(@PathVariable(name = "id") Long id) throws DataNotFoundException {
+        return new ResponseEntity<>(personSvc.readPersonById(id), HttpStatus.OK);
     }
 }
